40a1ad606e30c01186328f6dbe79c3a39b02bd68
Afifa Khan <khanaara5454@gmail.com>
2023-04-05 12:19:34 +0530

    added model prediction to API endpoint

--- views.py
+++ views.py
@@ -1,11 +1,13 @@
 import traceback
 import json
+import io
+import numpy as np
 
 from fastapi import HTTPException, Request, File
-
 import asyncio
 
 from models import User
+from model.densenet_waste_classifier import predict_class
 
 
 def test_view():
@@ -45,12 +47,14 @@ def reset_user_credentials(email: str, secret_question: str, secret_answer: str)
 
 async def classify_view(file: bytes = File()):
     try:
-        with open('test.jpg', 'wb+') as f:
-            f.write(file)
+        img = cv2.imdecode(np.frombuffer(file, np.uint8), -1)
 
-        pred = "Waste Type"
+        pred_status, pred_class = predict_class(img)
 
-        return {"predictedLabel": pred}
+        if not pred_status:
+            raise HTTPException(status_code=500, detail="Something went wrong.")
+
+        return {"predictedClass": pred_class}
     except Exception as e:
         traceback.print_exc()
         raise HTTPException(status_code=500, detail="Something went wrong.")

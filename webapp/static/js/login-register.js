3dc8ad1b9918d752953554bb855f464abaef0450
Afifa Khan <khanaara5454@gmail.com>
2023-04-06 23:59:20 +0530

    added login register forms in ui

---
+++ webapp/static/js/login-register.js
@@ -0,0 +1,9 @@
+function viewLoginForm() {
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("registration-form").style.display = "none";
+}
+
+function viewRegistrationForm() {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("registration-form").style.display = "block";
+}

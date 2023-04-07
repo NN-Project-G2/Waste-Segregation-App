bf4f95186da70100f187a15d094c423ea042a774
Afifa Khan <khanaara5454@gmail.com>
2023-04-07 15:20:07 +0530

    added reset password form

--- webapp/static/js/login-register.js
+++ webapp/static/js/login-register.js
@@ -1,9 +1,18 @@
 function viewLoginForm() {
     document.getElementById("login-form").style.display = "block";
     document.getElementById("registration-form").style.display = "none";
+    document.getElementById("reset-password-form").style.display = "none";
 }
 
 function viewRegistrationForm() {
     document.getElementById("login-form").style.display = "none";
     document.getElementById("registration-form").style.display = "block";
+    document.getElementById("reset-password-form").style.display = "none";
 }
+
+function viewResetPasswordForm() {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("registration-form").style.display = "none";
+    document.getElementById("reset-password-form").style.display = "block";
+}
+
